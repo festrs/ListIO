91f5a6d12011407a620a42bc6e485935e79c09ec
festrs <felipe.dipereira@gmail.com>
2016-05-30 15:55:32 -0300

    Initial Commit

---
+++ ListioTests/ListioTests.swift
@@ -0,0 +1,36 @@
+//
+//  ListioTests.swift
+//  ListioTests
+//
+//  Created by Felipe Dias Pereira on 2016-05-30.
+//  Copyright © 2016 Felipe Dias Pereira. All rights reserved.
+//
+
+import XCTest
+@testable import Listio
+
+class ListioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
